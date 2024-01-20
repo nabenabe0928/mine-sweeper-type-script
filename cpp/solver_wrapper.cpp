b459f17522a419172f67a19084f9f71e45771978
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2024-01-20 07:57:59 +0100

    Modify solver wrapper

--- cpp/solver_wrapper.cpp
+++ cpp/solver_wrapper.cpp
@@ -3,8 +3,18 @@
 
 using namespace emscripten;
 
+int get_size(vector<double>* vec) {
+  return vec->size();
+}
+
+double get_at(vector<double>* vec, int index) {
+  return (*vec)[index];
+}
+
 EMSCRIPTEN_BINDINGS(mine_sweeper_solver) {
   register_vector<int>("VectorInt");
   register_vector<double>("VectorDouble");
-  function("calculate_prob_flatten", &calculate_prob_flatten);
+  function("calculate_prob_flatten", &calculate_prob_flatten, allow_raw_pointers());
+  function("get_size", &get_size, allow_raw_pointers());
+  function("get_at", &get_at, allow_raw_pointers());  
 }
